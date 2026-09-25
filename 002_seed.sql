00634c2b8157a53d8d7c66f841642b2a41524a1f
vitenus <vitenusconsultoria@gmail.com>
2026-09-25 20:50:39 -0300

    Delete 002_seed.sql

--- 002_seed.sql
+++
@@ -1,3 +0,0 @@
-insert into methodology_versions(version_code,release_date,status,description,assumptions)
-values ('VITENUS-METHODOLOGY-0.1',current_date,'experimental','Prototype de diagnostic, valorisation circulaire, priorisation, Payback, ROI et mesure de performance.',jsonb_build_object('ipc_weights',jsonb_build_object('IE',.30,'FT',.20,'IA',.20,'CI',.10,'TN',.10,'GR',.10),'roi_bands_are_pilot_governance',true,'not_iso_certification',true))
-on conflict (version_code) do nothing;
